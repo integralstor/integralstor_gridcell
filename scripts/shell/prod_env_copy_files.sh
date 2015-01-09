7acd1303b5b4d0741480be4295ada9d500d9e5dc
ramaseshan <ramaseshanmukund@gmail.com>
2015-01-09 15:24:54 +0530

    Modified shell typo

--- scripts/shell/prod_env_copy_files.sh
+++ scripts/shell/prod_env_copy_files.sh
@@ -5,7 +5,7 @@ echo "Copying salt modules"
 cp -rf /tmp/integral_view/integral-view/monitoring/salt/_modules /srv/salt
 
 echo "Copying monitoring utilities"
-cp -rf /tmp/integral_view/integral-view/monitoring/python/ /opt/fractalio/monitoring/
+cp -rf /tmp/integral_view/integral-view/monitoring/python/ /opt/fractalio/scripts/
 
 echo "Updating Integral view -- Django app"
 cp -rf /tmp/integral_view/integral-view/integral_view/ /opt/fractalio/integral_view/
