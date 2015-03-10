9214776650a23bb74dea8b44c04573ce9def9e2c
root <root@localhost.localdomain>
2015-03-10 17:37:28 +0530

    Small typos

--- configuration_management/config/conf_files/mod_commands.sh
+++ configuration_management/config/conf_files/mod_commands.sh
@@ -7,9 +7,9 @@ modprobe ipmi_devintf
 echo "modprobe ipmi_devintf" >> /etc/rc.local
 chmod +x /etc/init.d/ramdisk
 chkconfig ramdisk on
-yes | rm -rf /etc/salt/pki/minion/minion_master.pub
-yes | rm /etc/salt/minion_id
 sed -i 's/\$ZFS mount -a/\$ZFS mount -O -a/' /etc/init.d/zfs
-sed -i 's/master: 10.1.1.200/master: fractalio-pri/' /etc/salt/minion
+sed -i 's/master: 10.1.1.200/master: fractalio-pri.fractalio.lan/' /etc/salt/minion
 rm -rf /tmp/*
-shutdown -r now
+yes | rm -f /etc/salt/pki/minion/minion_master.pub
+yes | rm -f /etc/salt/minion_id
+service salt-minion stop && shutdown -r now
