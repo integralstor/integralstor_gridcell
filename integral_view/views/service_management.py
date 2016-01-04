e0c96223b7fec78d5cb99a6159d6c2a93a5fb233
Ramaseshan <ramaseshan@ramaseshan.fractalio.lan>
2016-01-04 14:03:13 +0530

    cleanup services code and small bug fixes

--- integral_view/views/service_management.py
+++ integral_view/views/service_management.py
@@ -3,62 +3,68 @@ import django
 import salt.client
 from datetime import datetime
 from integralstor_common import scheduler_utils,common
+from integralstor_gridcell import services
+from integralstor_gridcell.services import default_services
 
-#Global Declaration of services so as to reused everywhere else
-services = {'Gluster':'glusterd','CIFS-CTDB':'ctdb','CIFS-Smb':'smb','CIFS-Winbind':'winbind','NTP':'ntpd','NFS':'rpcbind'}
 
-#Exception handlings to be written to all the code below 
+#Exception handlings to be written to all the code below
 def get_service_status(request):
   return_dict = {}
   return_dict['service_status'] = {}
-  client = salt.client.LocalClient()
-  for name,service in services.items():
-     status = client.cmd('*','service.status',[service])
-     if not status:
-       return_dict["error"] = "Unable to get status of %s(%s)"%(name,service)
-       return_dict["error_details"] = "You seem to have accessed this page by changing the url. This will not work as intended. Please go back to the old screen and redo the operation"
-       return django.shortcuts.render_to_response("logged_in_error.html", return_dict, context_instance=django.template.context.RequestContext(request))
-     return_dict['service_status'][name] = status
-  return django.shortcuts.render_to_response("view_service_status.html", return_dict, context_instance=django.template.context.RequestContext(request))
+  try:
+    service_status,err = services.get_service_status()
+    if not err:
+      return_dict['service_status'] = service_status
+      return django.shortcuts.render_to_response("view_service_status.html", return_dict, context_instance=django.template.context.RequestContext(request))
+    else:
+      return_dict["error"] = 'Unable to retrive the status of services'
+      return_dict["error_details"] = "An error occurred when processing your request : %s"%err
+      return django.shortcuts.render_to_response("logged_in_error.html", return_dict, context_instance=django.template.context.RequestContext(request))
+  except Exception, e:
+    return_dict["error"] = 'Unable to retrive the status of services'
+    return_dict["error_details"] = "An error occurred when processing your request : %s"%e
+    return django.shortcuts.render_to_response("logged_in_error.html", return_dict, context_instance=django.template.context.RequestContext(request))
+
 
 def node_service_action(request):
   return_dict = {}
-  if request.method == "POST":
-    node = request.POST.get('node')
-    action = 'service.'+request.POST.get('action')
-    service = request.POST.get('service')
-    service = services[service]
-    client = salt.client.LocalClient()
-    status = client.cmd(node,action,[service]) 
-    if not False in status.values():
-      return django.http.HttpResponseRedirect('/view_service_status/')
-    else:
-      return_dict["error"] = "%s %s failed."%(service,action)
-      return_dict["error_details"] = "The system could not locate a service with name '%s' .Please redo the operation. If this problem persists, contact for support."%service
-      return django.shortcuts.render_to_response("logged_in_error.html", return_dict, context_instance=django.template.context.RequestContext(request))
+  try:
+    if request.method == "POST":
+        service = request.POST.get('service')
+        action = request.POST.get('action')
+        node = request.POST.get('node')
+        status,err = services.service_action(node,default_services[service],action)
+        if err:
+          return_dict["error"] = "%s %s failed."%(service,action)
+          return_dict["error_details"] = "The system could not locate a service with name '%s' .Please redo the operation. If this problem persists, contact for support."%service
+          return django.shortcuts.render_to_response("logged_in_error.html", return_dict, context_instance=django.template.context.RequestContext(request))
 
-  else:
-    return_dict["error"] = "Please use the menus and screens to access this page"
-    return_dict["error_details"] = "You seem to have accessed this page by changing the url. This will not work as intended. Please go back to the old screen and redo the operation"
+        return django.http.HttpResponseRedirect('/view_service_status/?status=success&service=%s&action=%s&node=%s'%(service,action,node))
+    else:
+      return_dict["error"] = "Please use the menus and screens to access this page"
+      return_dict["error_details"] = "You seem to have accessed this page by changing the url. This will not work as intended. Please go back to the old screen and redo the operation"
+      return django.shortcuts.render_to_response("logged_in_error.html", return_dict, context_instance=django.template.context.RequestContext(request))
+  except Exception, e:
+    return_dict["error"] = 'Unable to retrive the status of services'
+    return_dict["error_details"] = "An error occurred when processing your request : %s"%s
     return django.shortcuts.render_to_response("logged_in_error.html", return_dict, context_instance=django.template.context.RequestContext(request))
 
 def initiate_scrub(request):
   return_dict = {}
-  if request.method == "GET":
-    return django.shortcuts.render_to_response("initiate_scrub.html", return_dict, context_instance=django.template.context.RequestContext(request))
-  elif request.method == "POST":
-    dt = request.POST.get('timestamp')
-    poolname = 'frzpool'
-    db_path,err = common.get_db_path()
-    status,err = scheduler_utils.schedule_a_job(db_path,'ZFS Scrub',[{'scrub':'zpool scrub frzpool'}],int(dt))
-    if err:
-      return_dict["error"] = "Scrub scheduling failed"
-      return_dict["error_details"] = err
-      return django.shortcuts.render_to_response("logged_in_error.html", return_dict, context_instance=django.template.context.RequestContext(request))
-    return django.shortcuts.render_to_response("initiate_scrub.html", return_dict, context_instance=django.template.context.RequestContext(request))
-
-
-
-
-    
-
+  try:
+    if request.method == "GET":
+      return django.shortcuts.render_to_response("initiate_scrub.html", return_dict, context_instance=django.template.context.RequestContext(request))
+    elif request.method == "POST":
+      dt = request.POST.get('timestamp')
+      poolname = 'frzpool'
+      db_path,err = common.get_db_path()
+      status,err = scheduler_utils.schedule_a_job(db_path,'ZFS Scrub',[{'scrub':'zpool scrub frzpool'}],int(dt))
+      if err:
+        return_dict["error"] = "Scrub scheduling failed"
+        return_dict["error_details"] = err
+        return django.shortcuts.render_to_response("logged_in_error.html", return_dict, context_instance=django.template.context.RequestContext(request))
+      return django.shortcuts.render_to_response("initiate_scrub.html", return_dict, context_instance=django.template.context.RequestContext(request))
+  except Exception, e:
+    return_dict["error"] = 'Unable to retrive the status of services'
+    return_dict["error_details"] = "An error occurred when processing your request : %s"%s
+    return django.shortcuts.render_to_response("logged_in_error.html", return_dict, context_instance=django.template.context.RequestContext(request))
