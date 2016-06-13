2906c7f0d2ca2a32b169ed4943c17459e3ee9233
naveen-fractlio <naveen.mh08@gmail.com>
2016-06-13 17:59:36 +0530

    Software and package upgrade script is added for upgrading existing system for new code.

---
+++ install/upgrade/update_software.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+touch /etc/yum.repos.d/fractalio.repo
+echo
+read -t 60 -p "Enter the IP from which the upgrade needs to be happen: " input
+echo "Entered IP is: " $input
+read -t 30 -p "Please Press 'yes' to Confirm: " confirm
+
+if [ "$confirm" == "yes" ]; then
+
+#Create and enable the fractalio.repo for pull
+cat << EOF > /etc/yum.repos.d/fractalio.repo
+[fractalio]
+enabled=1
+name= Fractalio- Updates
+baseurl=http://$input/netboot/distros/centos/6.6/x86_64/integralstor_gridcell/v1.0/rpms_zfs-0.6.5.6
+gpgcheck=0
+EOF
+cd /tmp
+wget -c http://$input/netboot/distros/centos/6.6/x86_64/integralstor_gridcell/v1.0/updater.sh
+chmod +x /tmp/updater.sh
+/tmp/updater.sh
+read -t 30 -p "Successfully Updated to Latest software. Please Press 'yes' to restart the system now: " again
+	if [ "$again" == "yes" ]; then
+		reboot now
+	else
+		echo "You must restart your server before the new settings will take effect."
+	fi
+else
+echo "Something went wrong. Please try again..."
+fi
+rm -f /tmp/updater.sh
+
+#Disable the fractalio.repo once the upgrade is done.
+cat << EOF > /etc/yum.repos.d/fractalio.repo
+[fractalio]
+enabled=0
+name= Fractalio- Updates
+baseurl=http://$input/netboot/distros/centos/6.6/x86_64/integralstor_gridcell/v1.0/rpms_zfs-0.6.5.6
+gpgcheck=0
+EOF
