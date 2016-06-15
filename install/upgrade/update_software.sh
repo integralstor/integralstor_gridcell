339a3f9404251d874742ecd2277350800ba01ab7
voidspacexyz <null@voidspace.xyz>
2016-06-15 12:14:19 +0530

    Cleanedout english for updater

--- install/upgrade/update_software.sh
+++ install/upgrade/update_software.sh
@@ -3,8 +3,8 @@
 touch /etc/yum.repos.d/fractalio.repo
 echo
 read -t 60 -p "Enter the IP from which the upgrade needs to be happen: " input
-echo "Entered IP is: " $input
-read -t 30 -p "Please Press 'yes' to Confirm: " confirm
+echo "Pulling updates from : " $input
+read -t 30 -p "Press 'yes' to Confirm: " confirm
 
 if [ "$confirm" == "yes" ]; then
 
@@ -20,7 +20,7 @@ cd /tmp
 wget -c http://$input/netboot/distros/centos/6.6/x86_64/integralstor_gridcell/v1.0/updater.sh
 chmod +x /tmp/updater.sh
 /tmp/updater.sh
-read -t 30 -p "Successfully Updated to Latest software. Please Press 'yes' to restart the system now: " again
+read -t 30 -p "Successfully Updated to Latest software. Press 'yes' to restart the system now: " again
 	if [ "$again" == "yes" ]; then
 		reboot now
 	else
