a3377ceca38698d68f96f387c71751050d30be56
naveen-fractlio <naveen.mh08@gmail.com>
2016-06-15 12:23:30 +0530

    Added updater script for package and code update

---
+++ install/upgrade/updater.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+echo "Updating the ZFS and Other modules..."
+yes | yum update zfs zfs-dkms spl spl-dkms >&2
+echo "Successfully Updated ZFS and other modules."
+echo
+echo "Started Upgrading Software..."
+# Setup IntegralStor Common
+cd /tmp
+rm -rf /tmp/integralstor_*
+/usr/bin/wget -c http://192.168.1.150/netboot/distros/centos/6.6/x86_64/integralstor_gridcell/v1.0/tar_installs/integralstor_common.tar.gz
+/bin/tar xzf integralstor_common.tar.gz
+yes | cp -rf /tmp/integralstor_common/site-packages/integralstor_common/* /opt/integralstor/integralstor_common/site-packages/integralstor_common
+
+# Setup IntegralStor GRIDCell
+cd /tmp
+/usr/bin/wget -c http://192.168.1.150/netboot/distros/centos/6.6/x86_64/integralstor_gridcell/v1.0/tar_installs/integralstor_gridcell.tar.gz
+/bin/tar xzf integralstor_gridcell.tar.gz
+yes | cp -rf /tmp/integralstor_gridcell/integral_view/* /opt/integralstor/integralstor_gridcell/integral_view
+yes | cp -rf /tmp/integralstor_gridcell/site-packages/integralstor_gridcell/* /opt/integralstor/integralstor_gridcell/site-packages/integralstor_gridcell
+#yes | cp -rf /opt/integralstor/integralstor_gridcell/config /opt/integralstor
+### Apply quota information on every reboot if quota is not applied ###
+echo "sh /opt/integralstor_gridcell/scripts/shell/set_pool_quota.sh" >> /etc/rc.local
+echo "Software updated successfully."
