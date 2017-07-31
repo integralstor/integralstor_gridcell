c0652a9e6700f5266755ae34e73ad318aa8061a4
six-k <ramsri.hp@gmail.com>
2017-07-31 21:31:07 +0530

    Fix mounting of admin volume
    If the node is part of the storage pool, mount from localhost.
    
    Signed-off-by: six-k <ramsri.hp@gmail.com>

--- scripts/python/mount_admin_volume.py
+++ scripts/python/mount_admin_volume.py
@@ -48,10 +48,56 @@ def sync_ctdb_files():
             shutil.copyfile('%s/lock/public_addresses' %
                             str(config_dir), '/etc/ctdb/public_addresses')
             is_change = True
+
+        lg, err = logger.get_script_logger(
+            'Admin volume mounter: Sync CTDB config files', '/var/log/integralstor/scripts.log', level=logging.DEBUG)
+        if is_change == True:
+            logger.log_or_print(
+                'ctdb related files were synced.', lg, level='info')
+            logger.log_or_print(
+                'Restarting ctdb.', lg, level='debug')
+            out, err = command.get_command_output(
+                'service ctdb restart', False, True)
+            if not err and out:
+                logger.log_or_print(
+                    'Service ctdb: %s' % out, lg, level='debug')
+            else:
+                logger.log_or_print(
+                    'Service ctdb error: %s' % err, lg, level='error')
+        elif is_change == False:
+            logger.log_or_print(
+                'ctdb related files are in sync.', lg, level='info')
+
     except Exception, e:
         return False, is_change, "Couldn't sync ctdb files: %s" % str(e)
     else:
         return True, is_change, None
+
+
+
+def assert_admin_vol_mount():
+    try:
+        # Check if the admin vol mount actually succeded by reading a file from the admin volume
+        # Tries 3 times. Bail out if unsuccessfull
+        config_dir, err = config.get_config_dir()
+        if err:
+            raise Exception(err)
+
+        iter = 3
+        while (iter):
+            iter -= 1
+            is_mounted, err = grid_ops.is_admin_vol_mounted_local()
+            if err or (is_mounted is False):
+                time.sleep(5)
+            elif is_mounted is True:
+                break
+        if iter < 1:
+            raise Exception("Admin volume is not mounted!")
+
+    except Exception, e:
+        return False, "Couldn't assert mount point access: %s" % str(e)
+    else:
+        return True, None
 
 
 def mount_and_configure():
@@ -66,6 +112,8 @@ def mount_and_configure():
         pog, err = grid_ops.is_part_of_grid()
         if err:
             raise Exception(err)
+
+        # If the localhost is part of the gridcell, proceed
         if pog:
             logger.log_or_print('Checking glusterd service', lg, level='debug')
             service = 'glusterd'
@@ -103,118 +151,88 @@ def mount_and_configure():
             if err:
                 raise Exception(err)
 
-            is_pooled, err = gluster_trusted_pools.get_peer_list()
+            is_pooled = False
+            peer_list, err = gluster_trusted_pools.get_peer_list()
+            if peer_list:
+                is_pooled = True
+
+            is_mounted = False
+            # mount only if the localhost is pooled
             if is_pooled:
-                mounted, err = grid_ops.is_admin_vol_mounted_local()
-                if not mounted:
-                    for admin_gridcell in admin_gridcells:
-                        reachable, err = networking.can_ping(admin_gridcell)
-                        if reachable:
-                            (ret, rc), err = command.execute_with_rc(
-                                'mount -t glusterfs %s:/%s %s' % (admin_gridcell, admin_vol_name, config_dir))
-                            if (not err) and (rc == 0):
-                                mounted = True
-                                sync, is_change, error = sync_ctdb_files()
-                                if error:
-                                    # It's only a best-effort, it will try next
-                                    # minute again.
-                                    pass
-                                if sync == False:
-                                    #raise Exception (err)
-                                    pass
-                                if sync == True and is_change == True:
-                                    logger.log_or_print(
-                                        'ctdb related files were synced.', lg, level='info')
-                                    logger.log_or_print(
-                                        'Restarting ctdb.', lg, level='debug')
-                                    out, err = command.get_command_output(
-                                        'service ctdb restart', False, True)
-                                    if not err and out:
-                                        logger.log_or_print(
-                                            'Service ctdb: %s' % out, lg, level='debug')
-                                    else:
-                                        logger.log_or_print(
-                                            'Service ctdb error : %s' % err, lg, level='error')
+                is_mounted, err = grid_ops.is_admin_vol_mounted_local()
+                if not is_mounted:
+                    str = 'Admin volume is not mounted. Will attempt to mount now.' 
+                    logger.log_or_print(str, lg, level='error')
 
-                                """
-                # Restart winbind
-                out, err = command.get_command_output('service winbind restart', False, True)
-		if not err and out:
-		  logger.log_or_print('Service winbind: %s'%out, lg, level='debug')
-		else:
-		  logger.log_or_print('Service winbind error : %s'%err, lg, level='error')
-                # Restart smb
-                out, err = command.get_command_output('service smb restart', False, True)
-		if not err and out:
-		  logger.log_or_print('Service smb: %s'%out, lg, level='debug')
-		else:
-		  logger.log_or_print('Service smb error : %s'%err, lg, level='error')
-                """
+                    # Try to mount
+                    (ret, rc), err = command.execute_with_rc(
+                        'mount -t glusterfs localhost:/%s %s' % (admin_vol_name, config_dir))
+                    if err:
+                        str = 'Mount from localhost failed.' 
+                        logger.log_or_print(str, lg, level='error')
+                    elif (not err) and (rc == 0):
+                        is_access, err = assert_admin_vol_mount()
+                        if err:
+                            raise Exception(err)
 
-                                # Restart nginx
-                                out, err = command.get_command_output(
-                                    'service nginx restart', False, True)
-                                if not err and out:
-                                    logger.log_or_print(
-                                        'Service nginx: %s' % out, lg, level='debug')
-                                else:
-                                    logger.log_or_print(
-                                        'Service nginx error : %s' % err, lg, level='error')
-                                # Restart uwsgi
-                                out, err = command.get_command_output(
-                                    'service uwsgi restart', False, True)
-                                if not err and out:
-                                    logger.log_or_print(
-                                        'Service uwsgi: %s' % out, lg, level='debug')
-                                else:
-                                    logger.log_or_print(
-                                        'Service uwsgi error : %s' % err, lg, level='error')
+                        sync, is_change, error = sync_ctdb_files()
+                        if error:
+                            # It's only a best-effort, it will try next
+                            # minute again.
+                            pass
+                        if sync == False:
+                            #raise Exception (err)
+                            pass
 
-                                if ag:
-                                    # Restart salt-master
-                                    out, err = command.get_command_output(
-                                        'service salt-master restart', False, True)
-                                    if not err and out:
-                                        logger.log_or_print(
-                                            'Service salt-master: %s' % out, lg, level='debug')
-                                    else:
-                                        logger.log_or_print(
-                                            'Service salt-master error : %s' % err, lg, level='error')
-                                    # Restart salt-minion
-                                    out, err = command.get_command_output(
-                                        'service salt-minion restart', False, True)
-                                    if not err and out:
-                                        logger.log_or_print(
-                                            'Service salt-minion: %s' % out, lg, level='debug')
-                                    else:
-                                        logger.log_or_print(
-                                            'Service salt-minion error : %s' % err, lg, level='error')
-                                break
+                        # Restart nginx
+                        out, err = command.get_command_output(
+                            'service nginx restart', False, True)
+                        if not err and out:
+                            logger.log_or_print(
+                                'Service nginx: %s' % out, lg, level='debug')
+                        else:
+                            logger.log_or_print(
+                                'Service nginx error : %s' % err, lg, level='error')
+                        # Restart uwsgi
+                        out, err = command.get_command_output(
+                            'service uwsgi restart', False, True)
+                        if not err and out:
+                            logger.log_or_print(
+                                'Service uwsgi: %s' % out, lg, level='debug')
+                        else:
+                            logger.log_or_print(
+                                'Service uwsgi error : %s' % err, lg, level='error')
+
+                        if ag:
+                            # Restart salt-master
+                            out, err = command.get_command_output(
+                                'service salt-master restart', False, True)
+                            if not err and out:
+                                logger.log_or_print(
+                                    'Service salt-master: %s' % out, lg, level='debug')
                             else:
-                                str = 'Mount from %s failed.' % admin_gridcell
-                                logger.log_or_print(str, lg, level='error')
-                    if not mounted:
-                        str = 'Failed to mount admin volume!'
-                        logger.log_or_print(str, lg, level='critical')
+                                logger.log_or_print(
+                                    'Service salt-master error : %s' % err, lg, level='error')
+                            # Restart salt-minion
+                            out, err = command.get_command_output(
+                                'service salt-minion restart', False, True)
+                            if not err and out:
+                                logger.log_or_print(
+                                    'Service salt-minion: %s' % out, lg, level='debug')
+                            else:
+                                logger.log_or_print(
+                                    'Service salt-minion error : %s' % err, lg, level='error')
+
+                    str = 'Admin vol is mounted'
+                    logger.log_or_print(str, lg, level='info')
+
+                # Admin volume is mounted, perform required checks
                 else:
                     sync, is_change, err = sync_ctdb_files()
                     if err:
                         raise Exception(err)
                     if sync == False:
                         raise Exception(err)
-                    if sync == True and is_change == True:
-                        logger.log_or_print(
-                            'ctdb related files were synced.', lg, level='info')
-                        logger.log_or_print(
-                            'Restarting ctdb.', lg, level='debug')
-                        out, err = command.get_command_output(
-                            'service ctdb restart', False, True)
-                        if not err and out:
-                            logger.log_or_print(
-                                'Service ctdb: %s' % out, lg, level='debug')
-                        else:
-                            logger.log_or_print(
-                                'Service ctdb error : %s' % err, lg, level='error')
 
                     logger.log_or_print('Checking services', lg, level='debug')
                     for service in ['nginx', 'ctdb']:
@@ -251,20 +269,10 @@ def mount_and_configure():
                         else:
                             logger.log_or_print(
                                 'Service uwsgi error : %s' % err, lg, level='error')
+
                     str = 'Admin volume is already mounted'
                     logger.log_or_print(str, lg, level='info')
 
-                '''
-	if mounted:
-	  path_dict = {'/etc/krb5.conf':'%s/lock/krb5.conf'%config_dir, '/etc/samba/smb.conf':'%s/lock/smb.conf'%config_dir, '/etc/ctdb/nodes':'%s/lock/smb.conf'%config_dir, '/etc/sysconfig/ctdb':'%s/lock/ctdb'%config_dir}
-	  for link, file in path_dict.items():
-	    if os.path.islink(link) or os.path.isfile(link):
-	      print link, 'exists so removing'
-	      os.remove(link)
-	    os.symlink(file, link)
-	else:
-	  raise Exception('Could not mount the admin volume')
-	'''
     except Exception, e:
         st = 'Error mounting admin volume : %s' % e
         logger.log_or_print(st, lg, level='critical')
